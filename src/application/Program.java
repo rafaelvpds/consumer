a3f30e300cd57060ec18b11d0258ce56c96ba8bc
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-06-28 14:09:14 -0300

    initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Product> listaProdutos = new ArrayList<>();
+
+		listaProdutos.add(new Product("tv", 900.00));
+		listaProdutos.add(new Product("Mouse", 50.00));
+		listaProdutos.add(new Product("Tablet", 350.50));
+		listaProdutos.add(new Product("HD case", 80.90));
+		double min = 100.0;
+
+		listaProdutos.removeIf(p -> p.getPrice() >= min);
+		for (Product p : listaProdutos) {
+			System.out.println(p);
+		}
+
+	}
+
+}
